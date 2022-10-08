14f5db8d9365bf57432ba73b9f53ec0946ee4f66
Jaroslav Petras <niekto@niekde.sk>
2022-10-08 20:11:12 +0200

    add log directory and set permissions

--- entrypoint.bash
+++ entrypoint.bash
@@ -2,14 +2,16 @@
 set -eE
 CONFD="/usr/local/etc/squid/conf.d"
 CDMPD="/srv/data/coredump"
+LOGSD="/srv/data/log"
 
 if ! [[ -n $(ls -1A ${CONFD}) ]]; then
   echo "ERROR: there are no configuration files to be included in directory: '${CONFD}' " >&2
   exit 100
 fi
 
-if ! [[ -d ${CDMPD} ]]; then
-  mkdir /srv/data/coredump
-fi
+if ! [[ -d ${CDMPD} ]]; then mkdir -p "${CDMPD}"; fi
+if ! [[ -d ${LOGSD} ]]; then mkdir -p "${LOGSD}"; fi
+chmod ug=rwX,o-rX -R "${CDMPD}"
+chmod ug=rwX,o-rX -R "${LOGSD}"
 
 exec /usr/sbin/squid -N -f "/usr/local/etc/squid/squid.conf"
